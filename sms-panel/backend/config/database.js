63e809b9ca01de1a400370590f372b942c00f7f0
CanOzseven <timbersaw01@hotmail.com>
2025-12-08 23:48:00 +0300

    Update database.js

--- sms-panel/backend/config/database.js
+++ sms-panel/backend/config/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
+require('dotenv').config();
 
 /**
  * MongoDB bağlantısını başlat ve ilk admin kullanıcısını oluştur
